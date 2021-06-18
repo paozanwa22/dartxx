b181151ea6cf902f188103dd28321a45c86ddbcb
paozanwa22 <fausanwadi@gmail.com>
2021-06-18 14:09:23 +0800

    Jika dia beda maka dia hilang

--- main.dart
+++ main.dart
@@ -1,3 +1,11 @@
 void main(){
+  var gdghsa = '';
+
+  nama = 'wqwqe';
+  
+  print('hello $nama');
+  print('hello Develope');
+  print('hello Develope');
+  print('hello Develope');
   print('hello Develope');
 }
